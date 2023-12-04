574f947dd65750241a0ab469fa207b5495772647
Wiktor Bandzyra <wiktor.bandzyra@assecosol.com>
2023-12-04 15:13:43 +0100

    init project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.share_ride {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+    requires java.sql;
+
+    opens com.example.share_ride to javafx.fxml;
+    exports com.example.share_ride;
+}
